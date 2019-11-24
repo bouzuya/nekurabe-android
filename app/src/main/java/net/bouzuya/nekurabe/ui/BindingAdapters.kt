6e7a902aba42bcf708f276af1dee9be24d3b035d
bouzuya <m@bouzuya.net>
2019-11-24 14:24:54 +0900

    add store list

---
+++ app/src/main/java/net/bouzuya/nekurabe/ui/BindingAdapters.kt
@@ -0,0 +1,50 @@
+package net.bouzuya.nekurabe.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import net.bouzuya.nekurabe.data.Store
+import net.bouzuya.nekurabe.databinding.StoreListItemBinding
+
+@BindingAdapter(
+    "storeList"
+)
+fun RecyclerView.setStoreList(
+    storeList: List<Store>
+) {
+    class BindingViewHolder(val binding: StoreListItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    class StoreListAdapter : RecyclerView.Adapter<BindingViewHolder>() {
+        private var _dataSet: List<Store> = emptyList()
+        var dataSet: List<Store>
+            get() = _dataSet
+            set(values) {
+                _dataSet = values
+                notifyDataSetChanged()
+            }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder =
+            BindingViewHolder(
+                StoreListItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+
+        override fun getItemCount(): Int = dataSet.size
+
+        override fun onBindViewHolder(holder: BindingViewHolder, position: Int) {
+            holder.binding.item = dataSet[position]
+        }
+    }
+
+    if (adapter == null) {
+        layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        adapter = StoreListAdapter()
+    }
+    (adapter as? StoreListAdapter)?.dataSet = storeList
+}
--- app/src/main/java/net/bouzuya/nekurabe/ui/StoreListViewModel.kt
+++ app/src/main/java/net/bouzuya/nekurabe/ui/StoreListViewModel.kt
@@ -1,7 +1,20 @@
 package net.bouzuya.nekurabe.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import net.bouzuya.nekurabe.data.Store
+import org.threeten.bp.OffsetDateTime
 
 class StoreListViewModel : ViewModel() {
-    val message = "StoreList"
+    // TODO:
+    private val _storeList = MutableLiveData<List<Store>>(
+        listOf(
+            Store(1, "name1", OffsetDateTime.now(), OffsetDateTime.now()),
+            Store(2, "name2", OffsetDateTime.now(), OffsetDateTime.now()),
+            Store(3, "name3", OffsetDateTime.now(), OffsetDateTime.now())
+        )
+    )
+    val storeList: LiveData<List<Store>> = _storeList
+
 }
