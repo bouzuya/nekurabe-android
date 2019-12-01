e197a0609174a675c517f9d681d5e861e54fd021
bouzuya <m@bouzuya.net>
2019-12-01 14:45:29 +0900

    add navigation for store new action

--- app/src/main/java/net/bouzuya/nekurabe/ui/StoreListFragment.kt
+++ app/src/main/java/net/bouzuya/nekurabe/ui/StoreListFragment.kt
@@ -8,9 +8,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import net.bouzuya.nekurabe.data.EventObserver
 import net.bouzuya.nekurabe.data.NekurabeDatabase
 import net.bouzuya.nekurabe.data.StoreRepository
 import net.bouzuya.nekurabe.databinding.StoreListFragmentBinding
+import net.bouzuya.nekurabe.ui.StoreListFragmentDirections.Companion.actionStoreListFragmentToStoreEditFragment
 
 class StoreListFragment : Fragment() {
     private val viewModel: StoreListViewModel by viewModels {
@@ -30,5 +33,9 @@ class StoreListFragment : Fragment() {
     ): View? = StoreListFragmentBinding.inflate(inflater, container, false).also { binding ->
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
+
+        viewModel.createEvent.observe(viewLifecycleOwner, EventObserver {
+            findNavController().navigate(actionStoreListFragmentToStoreEditFragment())
+        })
     }.root
 }
--- app/src/main/java/net/bouzuya/nekurabe/ui/StoreListViewModel.kt
+++ app/src/main/java/net/bouzuya/nekurabe/ui/StoreListViewModel.kt
@@ -5,10 +5,14 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+import net.bouzuya.nekurabe.data.Event
 import net.bouzuya.nekurabe.data.Store
 import net.bouzuya.nekurabe.data.StoreRepository
 
 class StoreListViewModel(private val storeRepository: StoreRepository) : ViewModel() {
+    private val _createEvent = MutableLiveData<Event<Unit>>()
+    val createEvent: LiveData<Event<Unit>> = _createEvent
+
     private val _storeList = MutableLiveData<List<Store>>()
     val storeList: LiveData<List<Store>> = _storeList
 
@@ -19,6 +23,6 @@ class StoreListViewModel(private val storeRepository: StoreRepository) : ViewMod
     }
 
     fun create() {
-        TODO()
+        _createEvent.value = Event(Unit)
     }
 }
