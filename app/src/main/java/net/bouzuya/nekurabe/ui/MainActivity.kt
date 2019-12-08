9aa297644f7d851f2e4f5faece349faac2732c9b
bouzuya <m@bouzuya.net>
2019-12-08 15:01:40 +0900

    add action bar behavior

--- app/src/main/java/net/bouzuya/nekurabe/ui/MainActivity.kt
+++ app/src/main/java/net/bouzuya/nekurabe/ui/MainActivity.kt
@@ -2,6 +2,9 @@ package net.bouzuya.nekurabe.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import net.bouzuya.nekurabe.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(findNavController())
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController().navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun findNavController(): NavController =
+        findNavController(R.id.main_nav_host_fragment)
+
 }
