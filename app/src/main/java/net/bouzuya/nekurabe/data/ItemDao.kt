b310295ae02360a1b02c2a0c120112c948ea2306
bouzuya <m@bouzuya.net>
2019-12-22 14:28:34 +0900

    add ItemDao#insert

--- app/src/main/java/net/bouzuya/nekurabe/data/ItemDao.kt
+++ app/src/main/java/net/bouzuya/nekurabe/data/ItemDao.kt
@@ -2,10 +2,14 @@ package net.bouzuya.nekurabe.data
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 
 @Dao
 interface ItemDao {
     @Query("SELECT * FROM items")
     fun findAll(): LiveData<List<Item>>
+
+    @Insert
+    suspend fun insert(item: Item): Long
 }
