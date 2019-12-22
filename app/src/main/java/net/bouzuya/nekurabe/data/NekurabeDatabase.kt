396259bc50a346160eeae4e9cb9d769ebb1d946b
bouzuya <m@bouzuya.net>
2019-12-22 13:24:04 +0900

    add Item entity

--- app/src/main/java/net/bouzuya/nekurabe/data/NekurabeDatabase.kt
+++ app/src/main/java/net/bouzuya/nekurabe/data/NekurabeDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 @TypeConverters(value = [OffsetDateTimeConverter::class])
-@Database(entities = [Store::class], version = 1)
+@Database(entities = [Item::class, Store::class], version = 2)
 abstract class NekurabeDatabase : RoomDatabase() {
     abstract fun storeDao(): StoreDao
 }
