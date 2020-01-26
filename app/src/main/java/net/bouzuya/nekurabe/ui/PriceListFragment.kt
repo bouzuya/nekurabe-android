36c4f62f21fc9c82e8d8b4734b6843a1a650d770
bouzuya <m@bouzuya.net>
2020-01-26 15:11:15 +0900

    feat: add navigation price list -> price edit

--- app/src/main/java/net/bouzuya/nekurabe/ui/PriceListFragment.kt
+++ app/src/main/java/net/bouzuya/nekurabe/ui/PriceListFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import net.bouzuya.nekurabe.data.EventObserver
 import net.bouzuya.nekurabe.databinding.PriceListFragmentBinding
+import net.bouzuya.nekurabe.ui.PriceListFragmentDirections.Companion.actionPriceListFragmentToPriceEditFragment
 import org.koin.android.ext.android.inject
 
 class PriceListFragment : Fragment() {
@@ -17,5 +20,9 @@ class PriceListFragment : Fragment() {
     ): View? = PriceListFragmentBinding.inflate(inflater, container, false).also { binding ->
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
+
+        viewModel.newEvent.observe(viewLifecycleOwner, EventObserver {
+            findNavController().navigate(actionPriceListFragmentToPriceEditFragment(0L))
+        })
     }.root
 }
--- app/src/main/java/net/bouzuya/nekurabe/ui/PriceListViewModel.kt
+++ app/src/main/java/net/bouzuya/nekurabe/ui/PriceListViewModel.kt
@@ -3,6 +3,7 @@ package net.bouzuya.nekurabe.ui
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import net.bouzuya.nekurabe.data.Event
 import net.bouzuya.nekurabe.data.Price
 import timber.log.Timber
 
@@ -10,8 +11,11 @@ class PriceListViewModel : ViewModel() {
     private val _priceList = MutableLiveData<List<Price>>(emptyList())
     val priceList: LiveData<List<Price>> = _priceList
 
+    private val _newEvent = MutableLiveData<Event<Unit>>()
+    val newEvent: LiveData<Event<Unit>> = _newEvent
+
     fun newPrice() {
-        TODO()
+        _newEvent.value = Event(Unit)
     }
 
     fun show(price: Price) {
