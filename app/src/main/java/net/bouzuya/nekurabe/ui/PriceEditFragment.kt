096b0d1363a42c920023e2064e008911b02ca35d
bouzuya <m@bouzuya.net>
2020-02-02 16:18:40 +0900

    feat: add navigation price edit -> price detail

--- app/src/main/java/net/bouzuya/nekurabe/ui/PriceEditFragment.kt
+++ app/src/main/java/net/bouzuya/nekurabe/ui/PriceEditFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import net.bouzuya.nekurabe.data.EventObserver
 import net.bouzuya.nekurabe.databinding.PriceEditFragmentBinding
+import net.bouzuya.nekurabe.ui.PriceEditFragmentDirections.Companion.actionPriceEditFragmentToPriceDetailFragment
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
 
@@ -20,5 +23,9 @@ class PriceEditFragment : Fragment() {
     ): View? = PriceEditFragmentBinding.inflate(inflater, container, false).also { binding ->
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
+
+        viewModel.savedEvent.observe(viewLifecycleOwner, EventObserver { savedId ->
+            findNavController().navigate(actionPriceEditFragmentToPriceDetailFragment(savedId))
+        })
     }.root
 }
