ee1b8d6054749c69e2899ce0c72cd21edfc34825
bouzuya <m@bouzuya.net>
2020-02-23 14:59:55 +0900

    fix: change delete button position in store detail fragment

--- app/src/main/java/net/bouzuya/nekurabe/ui/StoreDetailFragment.kt
+++ app/src/main/java/net/bouzuya/nekurabe/ui/StoreDetailFragment.kt
@@ -1,12 +1,11 @@
 package net.bouzuya.nekurabe.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import net.bouzuya.nekurabe.R
 import net.bouzuya.nekurabe.data.EventObserver
 import net.bouzuya.nekurabe.databinding.StoreDetailFragmentBinding
 import net.bouzuya.nekurabe.ui.StoreDetailFragmentDirections.Companion.actionStoreDetailFragmentToStoreEditFragment
@@ -20,16 +19,29 @@ class StoreDetailFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? =
-        StoreDetailFragmentBinding.inflate(inflater, container, false).also { binding ->
-            binding.lifecycleOwner = this
-            binding.viewModel = viewModel
+    ): View? = StoreDetailFragmentBinding.inflate(inflater, container, false).also { binding ->
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
 
-            viewModel.deleteCompleteEvent.observe(viewLifecycleOwner, EventObserver {
-                findNavController().navigateUp()
-            })
-            viewModel.editEvent.observe(viewLifecycleOwner, EventObserver { storeId ->
-                findNavController().navigate(actionStoreDetailFragmentToStoreEditFragment(storeId))
-            })
-        }.root
+        viewModel.deleteCompleteEvent.observe(viewLifecycleOwner, EventObserver {
+            findNavController().navigateUp()
+        })
+        viewModel.editEvent.observe(viewLifecycleOwner, EventObserver { storeId ->
+            findNavController().navigate(actionStoreDetailFragmentToStoreEditFragment(storeId))
+        })
+
+        setHasOptionsMenu(true) // onCreateOptionsMenu
+    }.root
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.store_detail, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == R.id.store_detail_destroy) {
+            viewModel.destroy()
+            true
+        } else super.onOptionsItemSelected(item)
+    }
 }
