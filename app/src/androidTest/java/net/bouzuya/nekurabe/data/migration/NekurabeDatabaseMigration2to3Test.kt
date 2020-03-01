36f0463e15cb5de3a224833238faa74289066678
bouzuya <m@bouzuya.net>
2020-03-01 15:02:49 +0900

    test: add database migration 2 to 3 tests

---
+++ app/src/androidTest/java/net/bouzuya/nekurabe/data/migration/NekurabeDatabaseMigration2to3Test.kt
@@ -0,0 +1,43 @@
+package net.bouzuya.nekurabe.data.migration
+
+import androidx.room.testing.MigrationTestHelper
+import androidx.test.platform.app.InstrumentationRegistry
+import com.jakewharton.threetenabp.AndroidThreeTen
+import net.bouzuya.nekurabe.data.NekurabeDatabase
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class NekurabeDatabaseMigration2to3Test {
+
+    @get:Rule
+    val migrationTestHelper = MigrationTestHelper(
+        InstrumentationRegistry.getInstrumentation(),
+        NekurabeDatabase::class.java.canonicalName
+    )
+
+    @Before
+    fun setUp() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        AndroidThreeTen.init(context)
+    }
+
+    @Test
+    fun migrate2to3() {
+        val databaseName = "test-database"
+
+        migrationTestHelper.createDatabase(databaseName, 2).also { database ->
+            testStoresTable(database)
+            testItemsTable(database)
+        }
+
+        migrationTestHelper.runMigrationsAndValidate(
+            databaseName,
+            3,
+            true,
+            NekurabeDatabaseMigration2to3()
+        ).also { database ->
+            testPricesTable(database)
+        }
+    }
+}
